b172dbf1d3a8dbf4ae2b6c0a4d1f7d0524e57fc2
Kumar Utsav <krum.utsav@gmail.com>
2024-01-09 14:21:07 +0530

    Update label_rules.py

--- leetcode/label_rules.py
+++ leetcode/label_rules.py
@@ -1,6 +1,6 @@
 import re
 
-from leetcode.utils.constants import LABEL_SPECIFICATION
+from leetcode.utils.constants import LABEL_SPECIFICATION, MISSING_TEXT
 
 
 def label_rule_for_company(text: str) -> str:
@@ -9,10 +9,10 @@ def label_rule_for_company(text: str) -> str:
         match = re.search(LABEL_SPECIFICATION["RE_COMPANY_SECONDARY"], text)
     if match:
         text = match.group("label").strip()
-        return text if len(text) > 1 else ""
-    return ""
+        return text if len(text) > 1 else MISSING_TEXT
+    return MISSING_TEXT
 
 
 def label_rule_for_others(text: str, label_type: str) -> str:
     match = re.search(LABEL_SPECIFICATION[f"RE_{label_type.upper()}"], text)
-    return match.group("label").strip() if match else ""
+    return match.group("label").strip() if match else MISSING_TEXT
