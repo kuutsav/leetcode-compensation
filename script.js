a7183db4caafb254a37edffd39baf25feb4f2aee
Nikhil Gupta <nikhil20530@iiitd.ac.in>
2024-06-24 01:06:01 +0530

    minor changes

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ let totalPages = 0;
 const svgWidth = 16;
 const svgHeight = 16;
 
-const svgCode = `
+const starSVG = `
     <svg fill="#000000" width="24px" height="24px" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" class="icon">
         <path d="M908.1 353.1l-253.9-36.9L540.7 86.1c-3.1-6.3-8.2-11.4-14.5-14.5-15.8-7.8-35-1.3-42.9 14.5L369.8 316.2l-253.9 36.9c-7 1-13.4 4.3-18.3 9.3a32.05 32.05 0 0 0 .6 45.3l183.7 179.1-43.4 252.9a31.95 31.95 0 0 0 46.4 33.7L512 754l227.1 119.4c6.2 3.3 13.4 4.4 20.3 3.2 17.4-3 29.1-19.5 26.1-36.9l-43.4-252.9 183.7-179.1c5-4.9 8.3-11.3 9.3-18.3 2.7-17.5-9.5-33.7-27-36.3z"/>
     </svg>
@@ -20,7 +20,7 @@ let globalFilterState = {
     searchString: '',
     yoeRange: [null, null], // Assuming null means no filter
     salaryRange: [null, null],
-    includeInterviewExp: false  // New property to filter by interview experience
+    includeInterviewExp: false  
 };
 
 const GLOBAL_ALLOWED_FILTERS=["company", "location", "mapped_role"];
@@ -51,7 +51,7 @@ function setStatsStr(data) {
     const endDate = data[nRecs - 1]?.creation_date;
     let statsStr = `Based on ${nRecs} recs parsed between ${startDate} and ${endDate} (only includes posts that were parsed successfully and had non-negative votes)`;
 
-    const legendStr = `<div style="text-align: center; margin-top: 10px;">${svgCode} Posts with Interview Experience</div>`;
+    const legendStr = `<div style="text-align: center; margin-top: 10px;">${starSVG} Posts with Interview Experience</div>`;
     
     // Wrap the stats string and the legend
     const content = `<div style="text-align: center;">${statsStr}</div>${legendStr}`;
@@ -254,8 +254,6 @@ function displayOffers(page) {
         });
     });
 
- 
-
     paginatedOffers.forEach((offer, index) => {
         const row = table.insertRow();
         const indexCell = row.insertCell();
@@ -268,7 +266,7 @@ function displayOffers(page) {
                         ${offer.id}
                     </a>
                     ${offer.interview_exp !== 'NA' ?
-                        `<span class="star-icon" style="cursor: pointer; margin-left: 5px;" onclick="window.open('${offer.interview_exp}', '_blank')">${svgCode}</span>` :
+                        `<span class="star-icon" style="cursor: pointer; margin-left: 5px;" onclick="window.open('${offer.interview_exp}', '_blank')">${starSVG}</span>` :
                         ''}
                 </abbr>
             </p>
@@ -297,7 +295,7 @@ function displayOffers(page) {
     const container = document.getElementById('offersTable');
     container.innerHTML = '';
     container.appendChild(table);
-    renderPageOptions(); // Render page options
+    renderPageOptions(); 
 
 }
 
@@ -376,9 +374,8 @@ function sortAndSliceData(companyCounts) {
     return [categories, counts];
 }
 
-// Event Listeners and Initial Setup
 document.addEventListener('DOMContentLoaded', async function () {
-    // Fetch and display offers
+    
     async function fetchOffers() {
         const response = await fetch('data/parsed_comps.json');
         const data = await response.json();
